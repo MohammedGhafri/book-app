33e93e698ec68e8fcf01e1b79b37a3c1dc2a97c8
MohammedGhafri <eng.m.ghafri@gmail.com>
2020-07-14 16:12:57 +0300

    add pass

--- public/app.js
+++ public/app.js
@@ -1,14 +1,31 @@
 'use strict';
 let a;
 // $('form').cl
-$('button').on('click',function(event){
+$('#search').on('click',function(event){
   // event.preventDefualt();
   // event.preventDefault();
 
-  $('button').toggleClass('search');
+  $(this).toggleClass('search');
   a=$(this).val();
   console.log("this from app",a);
 })
 $('.blue').on('click',function(){
   $(this).toggleClass('search');
+})
+$('.selectForm').hide();
+$('article').hide();
+
+
+$('#showMore').on('click',function(){
+     $(this).text(function(i, text){
+      return text === "Show More" ? "Show Less" : "Show More";
+  })
+  $(this).next().toggle();
+})
+$('.selectButton').on("click",function(){
+  $(this).text(function(i, text){
+    return text === "Select This Book" ? "I changed my Opinion" : "Select This Book";
+})
+  $(this).next().toggle();
+  console.log("mmmmm");
 })
